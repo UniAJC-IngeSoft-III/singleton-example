b3524e0ed3707ec8d3c05b8c892426b0cf936b70
aleshivan <aleshivan@users.noreply.github.com>
2014-09-03 09:21:47 -0500

    Changes for use int instead of AtomicInteger

--- src/main/java/edu/uniajc/ingesoft3/game/Martians.java
+++ src/main/java/edu/uniajc/ingesoft3/game/Martians.java
@@ -1,13 +1,11 @@
 package edu.uniajc.ingesoft3.game;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * Created by alexhd on 8/30/14.
  */
 public final class Martians {
     private static final Martians martians = new Martians();
-    private final AtomicInteger count = new AtomicInteger(10);
+    private int count = 10;
 
     private Martians(){
     }
@@ -17,22 +15,21 @@ public final class Martians {
     }
 
     synchronized public void killMartian(){
-        if(count.get() > 0){
-            count.decrementAndGet();
+        if(count > 0){
+            count--;
             System.out.println("Martian has killed");
         }
     }
 
     synchronized public void createMartian(){
-        if(count.get() > 0){
-            count.incrementAndGet();
+        if(count > 0){
+            count++;
             System.out.println("Martian has created");
         }
     }
 
     synchronized public int countMartians(){
-        int aliveMartians = count.get();
-        switch (aliveMartians){
+        switch (count){
             case 0:
                 System.out.println("You Win!!!");
                 break;
@@ -40,8 +37,8 @@ public final class Martians {
                 System.out.println("one Martian, remain in game");
                 break;
             default:
-                System.out.printf("%d Martians, remain in game", aliveMartians);
+                System.out.printf("%d Martians, remain in game", count);
         }
-        return aliveMartians;
+        return count;
     }
 }
